522a11a7b3f708f7e91d53aacd851009163de8ff
eugenf <eugenf78@gmail.com>
2018-05-12 09:03:16 +0300

    adding Router tests

--- Tests/SeagullTests/RouterTests.swift
+++ Tests/SeagullTests/RouterTests.swift
@@ -20,7 +20,28 @@ class RouterTests: XCTestCase {
             checkRoute(router.lookup(method: .GET, uri: path), path, .GET)
         }
     }
+
+    func testMethods() {
+        let methods: [HTTPMethod] = [.GET, .POST, .PUT, .DELETE]
+        methods.forEach { try! router.add(method: $0, relativePath: "/", handler: emptyHandler) }
+        
+        methods.forEach { (method) in
+            checkRoute(router.lookup(method: method, uri: "/"), "/", method)
+        }
+    }
     
+    func testParams() {
+        let routes = ["/user/:id", "/user/:id/:name", "/user/:id/vasya", "/auth/session/:id", "/:a/:b/:c/:d/:e/:f"]
+        routes.forEach { try! router.add(method: .GET, relativePath: $0, handler: emptyHandler) }
+        
+        checkRoute(router.lookup(method: .GET, uri: "/user/id123"), "/user/:id", .GET, ["id": "id123"])
+        checkRoute(router.lookup(method: .GET, uri: "/user/id123/petya"), "/user/:id/:name", .GET, ["id": "id123", "name": "petya"])
+        checkRoute(router.lookup(method: .GET, uri: "/user/id123/vasya"), "/user/:id/vasya", .GET, ["id": "id123"])
+        checkRoute(router.lookup(method: .GET, uri: "/auth/session/12"), "/auth/session/:id", .GET, ["id": "12"])
+        checkRoute(router.lookup(method: .GET, uri: "/a/b/c/d/e/f"), "/:a/:b/:c/:d/:e/:f", .GET, ["a": "a", "b": "b", "c": "c", "d": "d", "e": "e", "f": "f"])
+    }
+
+    // MARK: -
     func checkRoute(_ res: RouterResult, _ pattern: String, _ method: HTTPMethod, _ uriP: StringDict = [:], _ queryP: StringDict = [:]) {
         switch res {
         case .success(let value):

b0fc0605d031f7e96ffba4bdb0c010ea3955616e
eugenf <eugenf78@gmail.com>
2018-05-25 09:17:03 +0300

    fixing integration test

--- Tests/SeagullTests/EngineIntegrationTests.swift
+++ Tests/SeagullTests/EngineIntegrationTests.swift
@@ -20,17 +20,21 @@ class EngineIntegrationTests: XCTestCase {
     func testHelloWord() {
         let exp = XCTestExpectation()
         
-        let task = URLSession.shared.dataTask(with: URL(string: "http://localhost:9876/helloword")!) { (data, resp, err) in
+        let task = URLSession.shared.dataTask(with: URL(string: "http://localhost:9876/helloworld")!) { (data, resp, err) in
             let httpResp = resp as? HTTPURLResponse
             
             XCTAssertNil(err)
             XCTAssertEqual(200, httpResp?.statusCode)
             
+            XCTAssertEqual("text/plain", httpResp?.allHeaderFields["Content-Type"] as? String)
+            
+            let str = String(data: data!, encoding: .utf8)
+            XCTAssertEqual("Hello world!", str)
+            
             exp.fulfill()
         }
         
         task.resume()
-        
         wait(for: [exp], timeout: 1.0)
     }
 }

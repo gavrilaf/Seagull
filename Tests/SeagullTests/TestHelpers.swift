306c10c515a0c7f0f46f53768e846255fbbbe534
eugenf <eugenf78@gmail.com>
2018-07-01 19:21:21 +0300

    fixed GetFile test

--- Tests/SeagullTests/TestHelpers.swift
+++ Tests/SeagullTests/TestHelpers.swift
@@ -13,3 +13,17 @@ extension SgRequest {
         return SgRequest(pattern: pattern, uri: uri, method: method, headers: headers, urlParams: urlParams, queryParams: queryParams, body: body)
     }
 }
+
+func getResourcesPath(fileName: String, bundleClass: AnyClass) -> String {
+    let path = FileManager.default.currentDirectoryPath + "/" + fileName
+    #if os(Linux)
+        return path
+    #else
+        if FileManager.default.fileExists(atPath: path) {
+           return path
+        }
+    
+        let bundlePath = Bundle(for: bundleClass).resourcePath!
+        return bundlePath[bundlePath.startIndex..<bundlePath.range(of: "Seagull/")!.upperBound] + "/" + fileName
+    #endif
+}
